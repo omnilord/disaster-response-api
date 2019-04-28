e9000f6cf4f4f5b0c0ea8f6435411bb043a45873
Eric L. Truitte, MBA <omnilord@privateerconsulting.com>
2019-04-28 12:35:03 -0400

    Deny outstanding drafts when a Draftable is destroyed.

--- app/models/concerns/draftable.rb
+++ app/models/concerns/draftable.rb
@@ -4,10 +4,15 @@ module Draftable
   included do
     raise 'Woah there, partner!  You cannot include Draftable on Draft!' if self.class.name == 'Draft'
 
-    has_many :drafts, as: :draftable, dependent: :delete_all
+    has_many :drafts, as: :draftable
 
     scope :pending_drafts, -> { drafts.active.order(created_at: :desc) }
     scope :historic_drafts, -> { drafts.historic.order(created_at: :desc) }
+
+    # Deny outstanding drafts since the root object is destroyed
+    after_destroy do |obj|
+      obj.drafts.actionable.each(&:deny)
+    end
   end
 
   def draft_type
